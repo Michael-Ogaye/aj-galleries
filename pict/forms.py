6f4613b068765feab06f249ad344898b68518b6a
Michael Ogaye <hogaeymic@gmail.com>
2022-05-27 23:52:08 +0300

    category and location forms vcreated from model form

--- pict/forms.py
+++ pict/forms.py
@@ -1,3 +1,19 @@
+
 from django import forms
 from django.forms import ModelForm
-from .models import Image
+from .models import Image,Category,Location
+
+class ImageForm(ModelForm):
+    class Meta:
+        model=Image
+        fields='__all__'
+
+class CategoryForm(ModelForm):
+    class Meta:
+        model=Category
+        fields='__all__'
+
+class LoctionForm(ModelForm):
+    class Meta:
+        model=Location
+        fields='__all__'
