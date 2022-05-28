9fb89dbf62960230fc063545ac25e680aed5d5f6
Michael Ogaye <hogaeymic@gmail.com>
2022-05-28 15:07:29 +0300

    linking of the staticfiles

--- pict/urls.py
+++ pict/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 from . import views
 
 
-
 urlpatterns = [
     path('',views.home,name='home'),
 ]
@@ -13,3 +12,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    
